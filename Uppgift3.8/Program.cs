cf9c41d5b564f489880ef218bfe02b3f2a3492d5
arun06001 <arun06001@E-PW0234KE>
2023-10-06 10:45:08 +0200

    Add project files.

---
+++ Uppgift3.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv 3 olika ord på var sin rad");
+            string ord1 = Console.ReadLine();
+            string ord2 = Console.ReadLine();
+            string ord3 = Console.ReadLine();
+
+            if (ord1.CompareTo(ord2)<0 && ord1.CompareTo(ord3)<0)
+            {
+                Console.WriteLine($"{ord1} kommer först i bokstavsordning.");
+            }
+            else if (ord2.CompareTo(ord3) < 0)
+            {
+                Console.WriteLine($"{ord2} kommer först i bokstavsordning.");
+            }
+            else
+            {
+                Console.WriteLine($"{ord3} kommer först i bokstavsordning.");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta.");
+            Console.ReadKey();
+        }
+    }
+}
